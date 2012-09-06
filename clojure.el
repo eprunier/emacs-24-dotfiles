883887b7afca29f9ef03b7b395676b2b2088ddd1
Eric Prunier <eric.prunier@gmail.com>
2012-09-06 14:58:10 +0200

    Add clojure-mode for cljs files

--- clojure.el
+++ clojure.el
@@ -1,6 +1,7 @@
 (provide 'clojure)
 
 ;; Useful global settings as Emacs is used predominantely for Clojure development
+(add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
 
 ;; Launch the Clojure repl via Leiningen - M-x clojure-jack-in 
 ;; Global shortcut definition to fire up clojure repl and connect to it
