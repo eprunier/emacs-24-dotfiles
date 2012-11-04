50753754514cfe30a9237ab6434ad556f793a257
Eric Prunier <eric.prunier@gmail.com>
2012-11-04 17:26:41 +0100

    Remove starter-kit

--- clojure.el
+++ clojure.el
@@ -1,12 +1,13 @@
-(provide 'clojure)
-
 ;; Useful global settings as Emacs is used predominantely for Clojure development
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
-
-;; Launch the Clojure repl via Leiningen - M-x clojure-jack-in 
-;; Global shortcut definition to fire up clojure repl and connect to it
-(global-set-key (kbd "C-c C-j") 'clojure-jack-in)
 
 ;; Colour mach parens and other structure characters to make code easy to follow
 (global-rainbow-delimiters-mode)
 
+;; Enable paredit
+(require 'paredit)
+(defun turn-on-paredit () (paredit-mode 1))
+(add-hook 'clojure-mode-hook 'turn-on-paredit)
+
+
+(provide 'clojure)
--- init.el
+++ init.el
@@ -1,12 +1,9 @@
-;;;
-;;; custom
-;;; 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("baed08a10ff9393ce578c3ea3e8fd4f8c86e595463a882c55f3bd617df7e5a45" "54d1bcf3fcf758af4812f98eb53b5d767f897442753e1aa468cfeb221f8734f9" "01c5758d2d383c53dbfbae8c9a0bf78cd46e589ce83ae15b97c3147461408efa" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default))))
+ '(custom-safe-themes (quote ("d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,11 +11,13 @@
  ;; If there is more than one, they won't work right.
  )
 
+
 ;;
 ;; General
 ;;
 (setq make-backup-files nil)
 (setq-default truncate-lines nil)
+
 
 ;;
 ;; UTF-8 configuration
@@ -28,6 +27,7 @@
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 
+
 ;;
 ;; Disable mouse features
 ;;
@@ -35,35 +35,39 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-;; Add Marmalade package archive for Emacs starter kit and other Emacs packages
 
+;;
+;; Add default packages to the Emacs environment
+;; Packages are installed if they are not already present
+;;
 (require 'package)
 (add-to-list 'package-archives
-	'("marmalade" . "http://marmalade-repo.org/packages/") )
+	     '("marmalade" . "http://marmalade-repo.org/packages/") )
 (package-initialize)
 
-
-;; Add Clojure and other supporting packages to the Emacs environment
-;; Packages are installed if they are not already present
-;; The list includes packages for the starter kit, Clojure and markdown files (used by github)
-
 (when (not package-archive-contents)
- 	(package-refresh-contents))
+  (package-refresh-contents))
 
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-eshell starter-kit-bindings
-	clojure-mode clojure-test-mode
-        rainbow-delimiters
-        ac-slime slime slime-repl
-	markdown-mode
-        haskell-mode ghc ghci-completion
-        groovy-mode))
+(defvar my-packages '(paredit rainbow-delimiters highlight-parentheses
+			      clojure-mode clojure-test-mode
+			      ac-slime slime slime-repl nrepl
+			      markdown-mode
+			      haskell-mode ghc ghci-completion))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
+
 ;;
-;; customizing
+;; Default theme
+;;
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'zenburn)
+
+
+;;
+;; Customizing
 ;;
 (add-to-list 'load-path user-emacs-directory)
 (require 'custom-bindings)
@@ -71,9 +75,15 @@
 (require 'clojure)
 (require 'groovy)
 
-;;
-;; default theme
-;;
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'zenburn)
+;; Enable highlight-parentheses-mode on all buffers
+(define-globalized-minor-mode global-highlight-parentheses-mode
+  highlight-parentheses-mode
+  (lambda ()
+    (highlight-parentheses-mode t)))
+(global-highlight-parentheses-mode t)
 
+;; Enable paredit for Elisp
+(add-hook 'emacs-lisp-mode-hook 'turn-on-paredit)
+
+;; Show matching parentheses
+(show-paren-mode 1)
