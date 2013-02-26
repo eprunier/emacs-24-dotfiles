46df5d214807659354c11647d6fa12563e1d2843
Eric Prunier <eric.prunier@gmail.com>
2013-02-26 15:26:06 +0100

    Add Clojure nrepl-refresh function

--- custom-clojure.el
+++ custom-clojure.el
@@ -4,7 +4,7 @@
 ;; Colour mach parens and other structure characters to make code easy to follow
 (global-rainbow-delimiters-mode)
 
-;;
+;; Enable nRepl minor-mode for existing clojure buffers when nRepl connection is established
 (nrepl-enable-on-existing-clojure-buffers)
 
 ;; Enable paredit
@@ -12,5 +12,15 @@
 (defun turn-on-paredit () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
 
+;; Refresh nrepl-namespace ([org.clojure/tools.namespace] is required in project.clj)
+(defun nrepl-refresh ()
+  (interactive)
+  (set-buffer "*nrepl*")
+  (goto-char (point-max))
+  (insert "(require 'clojure.tools.namespace.repl)")
+  (nrepl-return)
+  (goto-char (point-max))
+  (insert "(clojure.tools.namespace.repl/refresh)")
+  (nrepl-return))
 
 (provide 'custom-clojure)
