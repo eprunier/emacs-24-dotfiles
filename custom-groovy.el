ffe223901f532f09c4aa08c64c1695a07990c051
Eric Prunier <eric.prunier@gmail.com>
2013-08-28 15:24:03 +0200

    Add Groovy mode as submodule

--- custom-groovy.el
+++ custom-groovy.el
@@ -1,9 +1,13 @@
+;;; Add Groovy mode to load-path
+(add-to-list 'load-path (concat user-emacs-directory "vendor/Emacs-Groovy-Mode"))
+
 ;;; turn on syntax highlighting
 (global-font-lock-mode 1)
 
 ;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
 (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
 
 ;;; make Groovy mode electric by default.
