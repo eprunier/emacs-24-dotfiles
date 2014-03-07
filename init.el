32c6513f06399884e3aac58856ab05f06a08de99
Eric Prunier <eric.prunier@gmail.com>
2014-03-07 21:34:26 +0100

    Add exec-path-from-shell module

--- init.el
+++ init.el
@@ -53,6 +53,7 @@
 			      cider ac-nrepl
 			      magit
 			      linum-off hlinum
+			      exec-path-from-shell
 			      markdown-mode))
 
 (dolist (package my-packages)
@@ -107,3 +108,6 @@
 
 ;; For non ascii characters
 (require 'iso-transl)
+
+;; Initialize exec-path-from-shell
+(exec-path-from-shell-initialize)
