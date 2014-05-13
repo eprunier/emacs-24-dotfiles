1b233b2be0ac952e1baf26466c9e018187ee242b
Eric Prunier <eric.prunier@gmail.com>
2014-05-13 18:10:56 +0200

    Disable gofmt

--- custom-go.el
+++ custom-go.el
@@ -1,11 +1,13 @@
 (defun go-config (go-root)
   (add-to-list 'load-path (concat go-root "/misc/emacs"))
   (require 'go-mode-load)
+
   (add-hook 'go-mode-hook
 	    (lambda ()
-	      (add-hook 'before-save-hook 'gofmt)
 	      (setq tab-width 4)
-	      (setq indent-tabs-mode 1))))
+	      (setq indent-tabs-mode 1)
+	      ;;(add-hook 'before-save-hook 'gofmt-before-save)
+	      )))
 
 (let ((go-root (getenv "GOROOT")))
   (when go-root
