18e571240f3b05cd77e4eba8efa0ae84a87fc0c8
Eric Prunier <eric.prunier@gmail.com>
2014-05-14 13:15:45 +0200

    Update go config to use GOROOT or GO_HOME env variables

--- custom-go.el
+++ custom-go.el
@@ -1,16 +1,16 @@
-(defun go-config (go-root)
-  (add-to-list 'load-path (concat go-root "/misc/emacs"))
+(defun go-config (goroot)
+  (add-to-list 'load-path (concat goroot "/misc/emacs"))
   (require 'go-mode-load)
 
   (add-hook 'go-mode-hook
 	    (lambda ()
 	      (setq tab-width 4)
 	      (setq indent-tabs-mode 1)
-	      (add-hook 'before-save-hook 'gofmt-before-save)
-	      )))
+	      (add-hook 'before-save-hook 'gofmt-before-save))))
 
-(let ((go-root (getenv "GOROOT")))
-  (when go-root
-    (go-config go-root)))
+(let ((goroot (or (getenv "GOROOT")
+		  (getenv "GO_HOME"))))
+  (when goroot
+    (go-config goroot)))
 
 (provide 'custom-go)
